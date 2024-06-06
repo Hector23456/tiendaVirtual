56e663e73adaa300017e43da6293ba001c842427
Hector <tanobernardini@gmail.com>
2024-06-06 14:34:13 -0300

    Initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ListPersonComponent } from '../list-person/list-person.component';
+import { CommonModule } from '@angular/common';  // Importa tu interfaz de usuario
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, ListPersonComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  users= [
+      "Jhon",
+     "Maria",
+    "Admin",
+     "pirates",
+    "призрак",
+    ]
+    
+    addUserToList(user: string) {
+      console.log(user);
+      
+      this.users.push(user)
+    }
+}
---
+++ src/app/productos/productos.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-productos',
+  standalone: true,
+  imports: [],
+  templateUrl: './productos.component.html',
+  styleUrls: ['./productos.component.css']
+})
+export class ProductosComponent {
+  
+}
